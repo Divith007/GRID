b8db38d78cbcaa72dc25ccb2e92d264eb2f10488
Divith S.D <divithsiruvai@gmail.com>
2025-07-18 15:58:41 +0530

    Remove JS menu toggle and fix CSS indentation
    Deleted the JavaScript code handling menu toggle functionality. Also corrected indentation in the CSS media query for the .logo class.

--- Js/script.js
+++
@@ -1,5 +0,0 @@
-console.log("JS file is working!");
-
-document.getElementById('menu-toggle')?.addEventListener('click', () => {
-  document.getElementById('nav-menu')?.classList.toggle('show');
-});
